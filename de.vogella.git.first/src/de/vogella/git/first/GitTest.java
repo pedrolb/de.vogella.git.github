4e435996d2953fc77b678c112738df5ad5d95c83
timoteobrasil <timoteo.brasil@gmail.com>
2013-10-09 08:55:45 -0300

    testando alteração de arquivo

--- de.vogella.git.first/src/de/vogella/git/first/GitTest.java
+++ de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -1,7 +1,8 @@
 package de.vogella.git.first;
 
 public class GitTest {
-	public static void main(String[] args) {
-		System.out.println("Git is cool");
-	}
+    public static void main(String[] args) {
+
+        System.out.println("Testando alteração de arquivo");
+    }
 }
